ca7814dffa428b916f9278946d6786199706d457
Eoin McMahon <eoin.mcmahon@teampicnic.com>
2026-07-10 08:46:12 +0200

    Add async-trait/syntect deps and StorageProvider abstraction over S3 client

--- src/local.rs
+++ src/local.rs
@@ -52,8 +52,8 @@ pub fn list_local(dir: &Path) -> Result<Vec<LocalEntry>> {
         }
     }
 
-    dirs.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
-    files.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+    dirs.sort_by_key(|a| a.name.to_lowercase());
+    files.sort_by_key(|a| a.name.to_lowercase());
     dirs.extend(files);
     Ok(dirs)
 }
